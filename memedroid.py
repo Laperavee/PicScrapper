6b09c576619b19b3b8cfadbeb63f32cab4c0f912
Laperavee <tomlebonpro@gmail.com>
2023-06-22 16:45:29 +0200

    second commit

---
+++ memedroid.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup as Bs
+import yaml
+import os
+from settings import *
+for i in range(1,100):
+    URL = f"https://fr.memedroid.com/memes/top/ever?page={i}"
+    largest_file = ""
+    largest_number = -1
+    folder_path = os.path.join(os.getcwd(), "images")
+    response = requests.get(url=URL)
+    html = response.text
+    soup = Bs(html, 'html.parser')
+    images_url = soup.find_all('picture')
+    for image in images_url:
+        image_source = image.find("img")
+        image_dict = {
+            'alt': image_source.get('alt'),
+            'src': image_source.get('src'),
+        }
+        try:
+            img_data = requests.get(image_dict['src']).content
+            filename = f'{folder_path}/{image_dict["alt"]}.jpg'
+            with open(filename, 'wb') as file:
+                file.write(img_data)
+            print(f"L'image {image_dict['alt']} a été téléchargée avec succès.")
+        except:
+            print(f"L'image {image_dict['alt']} n'a pas été téléchargée ")
+
